2e102fe1d341d06845cdc71b31e056c6d1c2126e
Sean Lynch <46963083+SeanLynch1@users.noreply.github.com>
2020-04-22 20:25:10 +0100

    First Commit, Connecting Unity Project to GitHub.

---
+++ GameTools2_2020/Assets/Scripts 1/playerInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+
+    
+    public event eventManager.InputEventFloat ForwardEvent;
+    public event eventManager.InputEventFloat YawEvent;
+    public event eventManager.InputEventFloat PitchEvent;
+    public event eventManager.InputEventFloat RollEvent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetKeyboardInput();
+    }
+
+    void GetKeyboardInput()
+    {
+        if (ForwardEvent != null)
+        {
+            if (Input.GetAxis("Throtle") != 0) ForwardEvent(Input.GetAxis("Throtle"));
+        }
+
+        if (PitchEvent != null)
+        {
+            if (Input.GetAxis("Vertical") != 0) PitchEvent(Input.GetAxis("Vertical"));
+
+        }
+        if (YawEvent != null)
+        {
+            if (Input.GetAxis("Horizontal") != 0) YawEvent(Input.GetAxis("Horizontal"));
+        }
+
+        if (RollEvent != null)
+        {
+            if (Input.GetAxis("Roll") != 0) RollEvent(Input.GetAxis("Roll"));
+        }
+        
+    }
+}
---
+++ GameTools2_2020/Assets/Scripts 1/shipController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipController : MonoBehaviour
+{
+
+    public playerInput playerInput;
+    public float forwardThrustPower = 100f;
+    public float yawSpeed = 10f;
+    public float pitchSpeed = 10f;
+    public float rollSpeed = 10f;
+    public float invertModifier = -1;
+    Rigidbody myRigidbody;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        playerInput.ForwardEvent += ForwardThrust;
+        playerInput.YawEvent += YawMovement;
+        playerInput.PitchEvent += PitchMovement;
+        playerInput.RollEvent += RollMovement;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ForwardThrust(float thrust)
+    {
+        myRigidbody.AddForce(gameObject.transform.forward * thrust * forwardThrustPower * Time.deltaTime);
+    }
+    public void YawMovement(float Yaw)
+    {
+        myRigidbody.AddTorque(gameObject.transform.up * Yaw * yawSpeed * Time.deltaTime);
+    }
+    public void PitchMovement(float pitch)
+    {
+        myRigidbody.AddTorque(gameObject.transform.right * pitch * invertModifier * pitchSpeed * Time.deltaTime);
+    }
+    public void RollMovement(float roll)
+    {
+        myRigidbody.AddTorque(gameObject.transform.forward * roll * rollSpeed * Time.deltaTime);
+    }
+
+}
